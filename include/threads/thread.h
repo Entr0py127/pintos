2efeb7ecfb398cf97c0c83a6f7adfc6b5f5d4da1
qwer906 <anstjdtn0227@gmail.com>
2025-11-07 21:43:04 +0900

    Fix: timer & thread logic update

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -91,6 +91,7 @@ struct thread {
 	enum thread_status status;          /* Thread state. */
 	char name[16];                      /* Name (for debugging purposes). */
 	int priority;                       /* Priority. */
+	int wake_tick;	// 일어날 tick 정보
 
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem;              /* List element. */
