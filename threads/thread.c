aa6c9caaa1554796fecefeeb4b6356a5e363410a
minjung0233 <minjung0233@gmail.com>
2025-11-08 20:22:32 +0900

    thread_set_priority 오류 수정

--- threads/thread.c
+++ threads/thread.c
@@ -328,26 +328,26 @@ thread_yield (void) {
 void
 thread_set_priority (int new_priority) {
 
-	// new_priority가 우선순위 기부로 들어온 우선순위가 아닌 경우
-	// 조건 : donations이 비어 있는 경우
+	int old_priority = thread_current ()->priority;
+	thread_current ()->original_priority = new_priority;
+	int changed_priority;
+
+	// donations이 비어 있는 경우
 	if(list_empty(&thread_current()->donations)){
-		thread_current ()->original_priority = new_priority;
-		thread_current()->priority = thread_current ()->original_priority;
+		changed_priority=new_priority;
 	}
-	// new_priority가 우선순위 기부로 들어온 우선순위인 경우
+	// donations이 비어 있지 않은 경우
+	// donations 리스트의 최대값과 new_priority 중 큰 거 넣기
 	else{
-		// 기증자(현재 스레드의 donations 중 맨앞에 있는거)
 		struct thread *donor = list_entry(list_front(&thread_current()->donations), struct thread, donation_elem);
-		// 현재 스레드의 우선순위를 새로운 (기부받은)우선순위로 변경
-		thread_current()->priority = 
-			(thread_current ()->original_priority > donor->priority) 
-				? thread_current ()->original_priority 
-				: donor->priority;
+		changed_priority= 
+			(new_priority > donor->priority) ? new_priority : donor->priority;
 	}
+	thread_current()->priority=changed_priority;
 
 	// priority가 낮아졌는데 ready_list에 더 높은 놈이 있으면 yield
-	if (new_priority < thread_current ()->original_priority && !list_empty(&ready_list) &&
-		list_entry(list_front(&ready_list), struct thread, elem)->priority > new_priority)
+	if (thread_current()->priority < old_priority && !list_empty(&ready_list) 
+		&& list_entry(list_front(&ready_list), struct thread, elem) > thread_current()->priority)
 		thread_yield();
 }
 
